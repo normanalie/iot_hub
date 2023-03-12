ed6a9712b07e74f346ea82a3dcaed6dec0fe40a6
Norman ALIE <mail@normanalie.fr>
2023-03-12 11:58:06 +0100

    MQTT API Struct

--- backend/routes/mqtt.js
+++ backend/routes/mqtt.js
@@ -1,13 +1,22 @@
-const express = require('express')
+const express = require('express');
 
 const router = express.Router();
 
-router.get('/devices/:id', (req, res)=>{
-    // Check if the device if registered for the user
-
+router.get('/devices/:id', (req, res) => {
+    // Check if the device is registered for the user
     // Subscribe to the update topic
-
     // Send an empty message to force device to publish state
+    // Schedule a cron to unsubscribe
+    // Read state and store in DB
+    // Return state
+});
 
-    
-})
+router.post('/devices/:id', (req, res) => {
+    // Check if the device is registered for th user
+    // Subscribe to the update topic
+    // Publish state on mqtt
+    // Schedule a cron to unsubscribe
+    // Read state and store in DB
+    // Compare request state and current state
+    // Return current state and comparasion
+});
