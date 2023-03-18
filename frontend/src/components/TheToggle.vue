47f7f6a7062668b5ab43dd0d0aa950f46684cfd4
Norman ALIE <mail@normanalie.fr>
2023-03-18 22:12:23 +0100

    Toggle bind

--- frontend/src/components/TheToggle.vue
+++ frontend/src/components/TheToggle.vue
@@ -1,6 +1,29 @@
+<script>
+export default {
+    props: {
+        modelValue: Boolean,
+    },
+    computed: {
+        model: {
+            get() {
+                return this.modelValue;
+            },
+            set(value) {
+                this.$emit('update:modelValue', value);
+            },
+        },
+    },
+};
+</script>
+
 <template>
     <label class="relative inline-flex items-center cursor-pointer">
-        <input type="checkbox" value="" class="sr-only peer" />
+        <input
+            type="checkbox"
+            :value="modelValue"
+            v-model="model"
+            class="sr-only peer"
+        />
         <div
             class="w-11 h-6 bg-slate-800 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 rounded-full peer dark:bg-slate-800 peer-checked:after:translate-x-full peer-checked:after:border-slate-100 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-slate-100 after:border-slate-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-slate-600 peer-checked:bg-purple-600"
         ></div>
--- frontend/src/components/cards/MagnetLampCard.vue
+++ frontend/src/components/cards/MagnetLampCard.vue
@@ -3,10 +3,20 @@ import TheToggle from '../TheToggle.vue';
 import ArrowRight from '../icons/ArrowRight.vue';
 import LightBulb from '../icons/LightBulb.vue';
 export default {
+    data() {
+        return {
+            is_on: false,
+        };
+    },
     components: {
         TheToggle,
         ArrowRight,
         LightBulb,
+    },
+    computed: {
+        fill() {
+            return this.is_on ? 'white' : 'none';
+        },
     },
 };
 </script>
@@ -16,12 +26,12 @@ export default {
         class="MagnetLampCard max-w-sm rounded-md overflow-hidden shadow-lg bg-slate-600 m-2 p-2 hover:bg-slate-500 flex"
     >
         <div class="content py-2 flex-1">
-            <LightBulb fill="none" />
+            <LightBulb :fill="fill" />
             <h1 class="inline-block bottom-0 text-lg">Magnet Lamp</h1>
 
             <p class="pt-2 pb-4 pl-0 text-stone-200 text-ms">Chambre</p>
 
-            <TheToggle />
+            <TheToggle v-model="is_on" />
         </div>
         <ArrowRight class="my-auto h-full" />
     </div>
